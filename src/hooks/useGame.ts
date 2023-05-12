63cbcac8139b2ea2b2ae3d694206179ac62a250d
Khant Sithu <khantseithuu@gmail.com>
2023-05-12 04:22:45 +0000

    refactor: custom hook

---
+++ src/hooks/useGame.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { Game } from "../entities/Game";
+import APIClient from "../services/api-client";
+
+const apiClient = new APIClient<Game>("/games");
+
+export const useGame = (slug: string) =>
+  useQuery({
+    queryKey: ["game", slug],
+    queryFn: () => apiClient.get(slug),
+  });
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import { Heading, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import { useGame } from "../hooks/useGames";
+import { useGame } from "../hooks/useGame";
 
 function GameDetailPage() {
   const { slug } = useParams();
