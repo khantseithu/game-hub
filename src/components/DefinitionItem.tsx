bdaf2d6e9c89ef97ca875c3f7847dbe202445f09
Khant Sithu <khantseithuu@gmail.com>
2023-05-12 05:03:59 +0000

    build game attributes

---
+++ src/components/DefinitionItem.tsx
@@ -0,0 +1,18 @@
+import { Box, Heading } from "@chakra-ui/react";
+import React from "react";
+
+type Props = {
+  term: string;
+  children: React.ReactNode | React.ReactNode[];
+};
+const DefinitionItem = ({ term, children }: Props) => {
+  return (
+    <Box marginY={5}>
+      <Heading as="dt" fontSize="md" color="gray.600">
+        {term}
+      </Heading>
+      <dd>{children}</dd>
+    </Box>
+  );
+};
+export default DefinitionItem;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,7 @@
 import { Heading, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
 import { useGame } from "../hooks/useGame";
 
 function GameDetailPage() {
@@ -15,6 +16,7 @@ function GameDetailPage() {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 }
